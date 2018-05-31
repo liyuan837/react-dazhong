52b2f506d4958bd793274271fc5886b170e28856
18260631810 <18260631810@163.com>
2018-06-01 00:24:22 +0800

    add：路由跳转

--- app/index.jsx
+++ app/index.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import {render} from 'react-dom'
+import {hashHistory} from 'react-router'
+
+import RouteMap from './router/routeMap'
 
 //公共样式
 import './static/css/common.less'
 
 import Hello from './containers/Hello'
 
-render(<Hello></Hello>,document.getElementById('root'))
+//性能测试
+import Perf from 'react-addons-perf'
+if(__DEV__){
+    window.Perf = Perf
+}
+
+// render(<Hello></Hello>,document.getElementById('root'))
+
+render(
+    <RouteMap history={hashHistory}/>,
+    document.getElementById('root')
+)
